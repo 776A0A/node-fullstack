e5fe519ad19701f612bfe6994b3e9020f1711f25
Darenome <38596865+776A0A@users.noreply.github.com>
2024-02-12 17:19:41 +0800

    feat: update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -7,21 +7,3 @@ declare type Nullable<T> = T | null | undefined
 declare type DeepNonNullable<T> = {
   [P in keyof T]: T[P] extends object ? DeepNonNullable<T[P]> : NonNullable<T[P]>
 }
-
-declare interface Direction {
-  down: boolean
-  left: boolean
-  right: boolean
-  up: boolean
-}
-
-declare interface Edge {
-  bottom?: number
-  left?: number
-  right?: number
-  top?: number
-}
-
-declare interface Injectable<T> {
-  inject(value: T): void
-}
